c45f6acc1a8dfb305a5ae3aa8b455f5697c3be6d
Shubhankar Tiwari <geniusshubhankartiwari@gmail.com>
2024-05-12 08:53:20 +0530

    First commit

---
+++ tailwindProps/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Signup from './Signup'
+
+function App() {
+  return (
+    <>
+    <Signup title="SignUp" link="Login"/>
+    <Signup title="LogIn" link="Signup"/>
+    </>
+  )
+}
+
+export default App
